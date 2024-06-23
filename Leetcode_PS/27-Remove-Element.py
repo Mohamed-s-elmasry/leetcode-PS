10c129d81b95cbb62a2dc8c3e5dcd582df82c1da
Mohamed-s-elmasry <148468988+Mohamed-s-elmasry@users.noreply.github.com>
2024-06-23 06:04:50 +0300

    [27] [Time Beats: 34.99%] [Memory Beats: 90.46%] - LeetPush

--- Leetcode_PS/27-Remove-Element.py
+++ Leetcode_PS/27-Remove-Element.py
@@ -5,10 +5,5 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        counter =0 
-        for  i in range(len(nums)):
-            if nums[i] != val :
-                nums[counter] = nums[i]
-                counter +=1
-            
-        return counter
+        while val in nums :
+            nums.remove(val)
